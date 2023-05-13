5f5cb57caa1e972deac726d287a335678a103ca8
SiZiOUS <sizious@gmail.com>
2023-05-13 20:55:55 +0200

    Removing `.S` rule as it's now fixed in KOS.

--- libfastmem/files/KOSMakefile.mk
+++ libfastmem/files/KOSMakefile.mk
@@ -3,8 +3,3 @@ OBJS = memcpy_fast.o memset_fast.o memmove_fast.o
 KOS_CFLAGS += -Iinclude
 
 include ${KOS_PORTS}/scripts/lib.mk
-
-# Override the default %.S rule from "Makefile.rules" (we want "kos-cc" instead
-# of "kos-as").
-%.o: %.S
-	kos-cc $< -o $@
