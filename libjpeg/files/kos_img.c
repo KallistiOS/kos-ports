e177ede7136debfb13190dc79f6006fec0fdfc6e
Andress Barajas <andress.barajas@gmail.com>
2023-12-23 19:01:09 -0800

    Remove debug

--- libjpeg/files/kos_img.c
+++ libjpeg/files/kos_img.c
@@ -72,8 +72,6 @@ int jpeg_to_img(const char *filename, int scale, kos_img_t * rv) {
     
 	/* Step 4: set parameters for decompression */
 	assert( scale == 1 || scale == 2 || scale == 4 || scale == 8 );
-
-	printf("SN: %d, SD: %d\n", cinfo.scale_num, cinfo.scale_denom);
 	cinfo.scale_num = 1;
 	cinfo.scale_denom = scale; /* must be 1, 2, 4, or 8 */
 
